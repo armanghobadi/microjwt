0a287a3c0f92d91b5a38a5bc5cd850d65b1ca011
Arman Ghobadi <132394098+armanghobadi@users.noreply.github.com>
2024-12-15 17:13:50 +0330

    Updated setup.py file

--- setup.py
+++ setup.py
@@ -2,14 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="microjwt",  # نام پکیج
-    version="0.1.0",  # نسخه اولیه
     description="A simple HMAC-based JWT implementation for MicroPython",
-    author="Arman Ghobadi",  # نام شما
-    author_email="arman.ghobadi.ag@gmai.com",  # ایمیل شما
-    url="https://github.com/armanghobadi/microjwt",  # لینک به مخزن گیت‌هاب یا وب‌سایت
-    packages=find_packages(),  # پیدا کردن همه بسته‌ها
-    install_requires=[  # لیست کتابخانه‌هایی که نیاز دارید (در صورت وجود)
-        "ubinascii",  # باید این را برای MicroPython اضافه کنید
+    author="Arman Ghobadi",  
+    author_email="arman.ghobadi.ag@gmai.com",  
+    url="https://github.com/armanghobadi/microjwt",  
+    packages=find_packages(),  
+    install_requires=[  
+        "ubinascii",  
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
